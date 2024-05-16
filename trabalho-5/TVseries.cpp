ee0bae8b9c3cf7756f7cf1da9f6f46f3eb0c4df8
DiogoRibeiro13 <164203784+DiogoRibeiro13@users.noreply.github.com>
2024-05-16 10:07:17 +0100

    Update TVseries.cpp

--- trabalho-5/TVseries.cpp
+++ trabalho-5/TVseries.cpp
@@ -4,78 +4,98 @@
 #include <functional>
 #include <stack>
 
-TVSeriesAPP::TVSeriesAPP() {
-
+TVSeriesAPP::TVSeriesAPP()
+{
+    auto TitleBasicsMap = unordered_map<string, TitleBasics>();
+    auto TitleEpisodesMap = unordered_map<string, TitleEpisode>();
+    auto TitlePrincipalsMap = unordered_map<string, TitlePrincipals>();
 }
-   
-TVSeriesAPP::~TVSeriesAPP() {
+
+
+TVSeriesAPP::~TVSeriesAPP()
+{
+    TitleBasicsMap.clear();
+    TitleEpisodesMap.clear();
+    TitlePrincipalsMap.clear();
+}
+
+
+
+
+
+void TVSeriesAPP::addTitleBasics(const TitleBasics& title)
+{
     
 }
 
 
-void TVSeriesAPP::addTitleBasics(const TitleBasics& title) {
-    
- }
-void TVSeriesAPP::addTitleEpisodes(const TitleEpisode& episode) {
+void TVSeriesAPP::addTitleEpisodes(const TitleEpisode& episode)
+{
   
 }
 
 
-void TVSeriesAPP::addTitlePrincipal(const TitlePrincipals& principal) {
+void TVSeriesAPP::addTitlePrincipal(const TitlePrincipals& principal)
+{
 
-    
 }
 
 
 
-//Pergunta 1:
 
-vector<string> TVSeriesAPP::getUniquePrincipals(const string& seriesTconst ) const {
-    
-    
 
+//PERGUNTA 1:
+
+vector<string> TVSeriesAPP::getUniquePrincipals(const string& seriesTconst ) const
+{
     return {};
 }
 
+
+
+
+
+
+
+
+
+
 //PERGUNTA 2:
 
-
-
-string TVSeriesAPP::getMostSeriesGenre() const {
-   
-
+string TVSeriesAPP::getMostSeriesGenre() const
+{
     return "";
 }
+
+
+
+
+
+
+
+
 
 
 //PERGUNTA 3: 
 
-string TVSeriesAPP::getPrincipalFromCharacter(const string& character) const {
-
+string TVSeriesAPP::getPrincipalFromCharacter(const string& character) const
+{
     return "";
 }
+
+
+
+
+
+
+
 
 
 
 //PERGUNTA 4
 
-vector<string> TVSeriesAPP::principalsWithMultipleCategories(const string& seriesTconst ) const {
-
-  return {};
-}
- 
-//PERGUNTA 5:
-
-int TVSeriesAPP::principalInMultipleGenres(vector<string> vGenres) {
-
-
-return -1;
-}
-
-
-//PERGUNTA 6: 
-vector<string> TVSeriesAPP::principalsInAllEpisodes(const string& seriesTconst) const {
-
+vector<string> TVSeriesAPP::principalsWithMultipleCategories(const string& seriesTconst ) const
+{
     return {};
 }
 
@@ -88,6 +108,24 @@ vector<string> TVSeriesAPP::principalsInAllEpisodes(const string& seriesTconst) 
 
 
 
+//PERGUNTA 5:
+
+int TVSeriesAPP::principalInMultipleGenres(vector<string> vGenres)
+{
+    return -1;
+}
 
 
 
+
+
+
+
+
+
+
+//PERGUNTA 6: 
+vector<string> TVSeriesAPP::principalsInAllEpisodes(const string& seriesTconst) const
+{
+    return {};
+}
